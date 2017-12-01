4c0a77a18fb761f10d0f0efb10fe06221eb0c2ad
sripakalapati <sripakalapati@gmail.com>
2017-12-01 13:14:48 -0500

    3rd commit

---
+++ src/main/scala/sparkscalapackage/RunRelationalNumber.scala
@@ -0,0 +1,20 @@
+package sparkscalapackage
+
+object RunRationalNumber {
+  def main(args: Array[String]): Unit ={
+    val firstnumber = new RationalNumber(1,3)
+    val secondnumber = new RationalNumber(1,6)
+    val resultnumber = firstnumber.add(secondnumber)
+    val subofnumbers = firstnumber.sub(secondnumber)
+    val mulofnumbers = firstnumber.mul(secondnumber)
+    val divofnumbers = firstnumber.div(secondnumber)
+
+    println("First Rational Number:  "+firstnumber)
+    println("Second Rational Number: "+secondnumber)
+    println("Addition of two Rational Numbers: "+resultnumber)
+    println("Substraction of two Rational Numbers: "+subofnumbers)
+    println("Multiplication of two Rational Numbers: "+mulofnumbers)
+    println("Division of two Rational Numbers: "+divofnumbers)
+
+  }
+}
